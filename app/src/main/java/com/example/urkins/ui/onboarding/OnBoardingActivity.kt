c49e9f17684d3a48df96118315c5b170b845a71f
vtw <vtiaraw2@gmail.com>
2024-11-30 12:13:33 +0700

    refactor: finishing onboarding

--- app/src/main/java/com/example/urkins/ui/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/example/urkins/ui/onboarding/OnBoardingActivity.kt
@@ -1,5 +1,6 @@
 package com.example.urkins.ui.onboarding
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowInsets
@@ -8,17 +9,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.urkins.MainActivity
 import com.example.urkins.R
+import com.example.urkins.databinding.ActivityOnBoardingBinding
 
 class OnBoardingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOnBoardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_on_boarding)
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        supportActionBar?.hide()
+
+        binding.btnGetStarted.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(
+                intent
+            )
+            finish()
         }
     }
 
