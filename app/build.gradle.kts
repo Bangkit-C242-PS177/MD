4cdcbf57e1be3711e3172ab01f53c425f8638d4b
AGUN1505 <agun1505@gmail.com>
2024-12-03 21:57:36 +0700

    add library glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,6 @@ dependencies {
     implementation(libs.converter.gson)
     implementation(libs.logging.interceptor)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
