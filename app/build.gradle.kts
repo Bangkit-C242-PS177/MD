075721f34c2e228bb61fa4195b090bd4b8259498
AGUN1505 <agun1505@gmail.com>
2024-12-04 00:09:43 +0700

    refactor library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
@@ -82,6 +82,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
 
     implementation(libs.glide)
-    implementation(libs.compiler)
+    annotationProcessor(libs.compiler)
     implementation(libs.okhttp3.integration)
 }
