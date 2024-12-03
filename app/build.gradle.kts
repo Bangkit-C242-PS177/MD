7150fb5cc3144cec97a30120c3bfd27fec197d6f
AGUN1505 <agun1505@gmail.com>
2024-12-03 22:07:30 +0700

    change to catalog version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,5 +77,5 @@ dependencies {
 
     implementation(libs.glide)
     implementation(libs.compiler)
-    implementation("com.github.bumptech.glide:okhttp3-integration:4.16.0")
+    implementation(libs.okhttp3.integration)
 }
