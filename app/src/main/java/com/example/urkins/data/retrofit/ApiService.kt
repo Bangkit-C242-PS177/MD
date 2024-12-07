c4551e19fd811d700b74580d41962d52d8fc5d0e
AGUN1505 <agun1505@gmail.com>
2024-12-07 21:40:01 +0700

    add function registeruser in api service

--- app/src/main/java/com/example/urkins/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/urkins/data/retrofit/ApiService.kt
@@ -1,4 +1,17 @@
 package com.example.urkins.data.retrofit
 
+import com.example.urkins.data.response.RegisterResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface ApiService {
+    @FormUrlEncoded
+    @POST("auth/register")
+    suspend fun registerUser(
+        @Field("username") username: String,
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("confirm_password") confirm_password: String,
+    ): RegisterResponse
 }
