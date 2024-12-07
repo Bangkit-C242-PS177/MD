eff3d732c02e3d497dfab0021d0b7253bc6279b5
AGUN1505 <agun1505@gmail.com>
2024-12-07 21:27:47 +0700

    add  function register repository in injection

--- app/src/main/java/com/example/urkins/di/Injection.kt
+++ app/src/main/java/com/example/urkins/di/Injection.kt
@@ -1,4 +1,16 @@
 package com.example.urkins.di
 
+import android.content.Context
+import com.example.urkins.data.pref.UserPreference
+import com.example.urkins.data.repository.RegisterRepository
+import com.example.urkins.data.retrofit.ApiConfig
+import kotlinx.coroutines.runBlocking
+
 object Injection {
+    fun registerRepository(context: Context): RegisterRepository {
+        val pref = UserPreference.getInstance(context.dataStore)
+        val user = runBlocking { pref.getUserToken() }
+        val apiService = ApiConfig.getApiService(user)
+        return RegisterRepository.getInstance(apiService)
+    }
 }
