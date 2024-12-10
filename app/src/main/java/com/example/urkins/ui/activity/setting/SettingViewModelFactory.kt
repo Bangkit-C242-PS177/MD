7cc66909c7f8b22cdcfab28d6b6c1abc18f2f763
AGUN1505 <agun1505@gmail.com>
2024-12-10 23:16:56 +0700

    add code in setting vm factory

--- app/src/main/java/com/example/urkins/ui/activity/setting/SettingViewModelFactory.kt
+++ app/src/main/java/com/example/urkins/ui/activity/setting/SettingViewModelFactory.kt
@@ -1,4 +1,35 @@
 package com.example.urkins.ui.activity.setting
 
-class SettingViewModelFactory {
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import com.example.urkins.data.repository.UserRepository
+import com.example.urkins.di.Injection
+import com.example.urkins.ui.activity.splashscreen.SplashViewModel
+
+class SettingViewModelFactory(private val repository: UserRepository) {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SplashViewModel::class.java)) {
+            return SplashViewModel(repository) as T
+        } else {
+            throw IllegalArgumentException("viewmodel class tidak ditemukan" + modelClass.name)
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SettingViewModelFactory? = null
+
+        @JvmStatic
+        fun getInstance(context: Context): SettingViewModelFactory {
+            if (INSTANCE == null) {
+                synchronized(SettingViewModelFactory::class.java) {
+                    INSTANCE = SettingViewModelFactory(Injection.provideRepository(context))
+                }
+            }
+            return INSTANCE as SettingViewModelFactory
+        }
+    }
 }
+
+
