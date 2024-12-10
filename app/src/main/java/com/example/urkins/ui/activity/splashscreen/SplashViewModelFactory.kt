6abf178e8f18ead38a1736a026a3e55c95cd1cfe
AGUN1505 <agun1505@gmail.com>
2024-12-10 16:30:28 +0700

    add  code in spalshfactory

--- app/src/main/java/com/example/urkins/ui/activity/splashscreen/SplashViewModelFactory.kt
+++ app/src/main/java/com/example/urkins/ui/activity/splashscreen/SplashViewModelFactory.kt
@@ -1,4 +1,17 @@
 package com.example.urkins.ui.activity.splashscreen
 
-class SplashViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class SplashViewModelFactory(private val userPreference: UserPreference) :
+    ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SplashViewModel::class.java)) {
+            return SplashViewModel(userPreference) as T
+        } else {
+            throw IllegalArgumentException("viewmodel class tidak ditemukan" + modelClass.name)
+        }
+    }
+
 }
