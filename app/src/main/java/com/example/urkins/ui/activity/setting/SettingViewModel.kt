368191e224667e79fc8b4ff4595fa784c83c3cf4
AGUN1505 <agun1505@gmail.com>
2024-12-10 23:19:36 +0700

    add logic for logout

--- app/src/main/java/com/example/urkins/ui/activity/setting/SettingViewModel.kt
+++ app/src/main/java/com/example/urkins/ui/activity/setting/SettingViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.urkins.ui.activity.setting
 
-class SettingViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.urkins.data.repository.UserRepository
+import kotlinx.coroutines.launch
+
+class SettingViewModel (private val repository: UserRepository) : ViewModel(){
+    fun logout() {
+        viewModelScope.launch {
+            repository.logout()
+        }
+    }
 }
