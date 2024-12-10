e766175e8f8262e6d8ddb274c60f4efa53232a0d
AGUN1505 <agun1505@gmail.com>
2024-12-10 23:18:03 +0700

    fixing code in setting vm factory

--- app/src/main/java/com/example/urkins/ui/activity/setting/SettingViewModelFactory.kt
+++ app/src/main/java/com/example/urkins/ui/activity/setting/SettingViewModelFactory.kt
@@ -2,11 +2,13 @@ package com.example.urkins.ui.activity.setting
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.urkins.data.repository.UserRepository
 import com.example.urkins.di.Injection
 import com.example.urkins.ui.activity.splashscreen.SplashViewModel
 
-class SettingViewModelFactory(private val repository: UserRepository) {
+class SettingViewModelFactory(private val repository: UserRepository):
+    ViewModelProvider.NewInstanceFactory() {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(SplashViewModel::class.java)) {
