a6e287fd9feed63c1fff01890c7fb4ee50435c5b
AGUN1505 <agun1505@gmail.com>
2024-12-11 00:37:34 +0700

    add code in historydao

--- app/src/main/java/com/example/urkins/data/local/room/HistoryDao.kt
+++ app/src/main/java/com/example/urkins/data/local/room/HistoryDao.kt
@@ -1,4 +1,24 @@
 package com.example.urkins.data.local.room
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.urkins.data.local.entity.HistoryEntity
+
+@Dao
 interface HistoryDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun saveResult(result: HistoryEntity): Long
+
+    @Query("SELECT * FROM HistoryEntity WHERE imageUri = :imageUri LIMIT 1")
+    fun getResultByImageUri(imageUri: String): HistoryEntity?
+
+    @Query("SELECT * FROM HistoryEntity")
+    fun showResult() : LiveData<List<HistoryEntity>>
+
+    @Delete
+    fun deleteResult(result: HistoryEntity)
 }
