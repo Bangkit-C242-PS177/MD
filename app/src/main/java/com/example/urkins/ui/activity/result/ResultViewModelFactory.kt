c92039a8c9241135faf07fa794daf379afe5b8f3
AGUN1505 <agun1505@gmail.com>
2024-12-11 10:57:38 +0700

    add  code in result vm factory

--- app/src/main/java/com/example/urkins/ui/activity/result/ResultViewModelFactory.kt
+++ app/src/main/java/com/example/urkins/ui/activity/result/ResultViewModelFactory.kt
@@ -1,4 +1,31 @@
 package com.example.urkins.ui.activity.result
 
-class ResultViewModelFactory {
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.urkins.data.repository.HistoryRepository
+import com.example.urkins.di.Injection
+
+class ResultViewModelFactory private constructor(
+    private val historyRepository: HistoryRepository
+) : ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ResultViewModel::class.java)) {
+            return ResultViewModel(historyRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
+
+    companion object {
+        @Volatile
+        private var instance: ResultViewModelFactory? = null
+
+        fun getInstance(context: Context): ResultViewModelFactory =
+            instance ?: synchronized(this) {
+                instance ?: ResultViewModelFactory(
+                    Injection.provideRepository(context)
+                )
+            }.also { instance = it }
+    }
 }
