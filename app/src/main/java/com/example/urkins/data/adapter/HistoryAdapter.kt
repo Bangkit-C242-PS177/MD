af5eda3c8ff8946da7550f648187f8782674954b
AGUN1505 <agun1505@gmail.com>
2024-12-11 09:40:47 +0700

    add code in history adapter

--- app/src/main/java/com/example/urkins/data/adapter/HistoryAdapter.kt
+++ app/src/main/java/com/example/urkins/data/adapter/HistoryAdapter.kt
@@ -1,4 +1,59 @@
 package com.example.urkins.data.adapter
 
-class HistoryAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.urkins.data.local.entity.HistoryEntity
+import com.example.urkins.databinding.ItemHistoryBinding
+
+class HistoryAdapter(
+    private val onDeleteClick: (HistoryEntity) -> Unit
+) : ListAdapter<HistoryEntity, HistoryAdapter.MyViewHolder>(DIFF_CALLBACK) {
+    class MyViewHolder(
+        private val binding: ItemHistoryBinding,
+        private val onDeleteClick: (HistoryEntity) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(history: HistoryEntity) {
+            binding.tvAnalyzeTime.text = history.prediction
+            binding.tvAnalyzeResult.text = history.prediction2
+            Glide.with(itemView.context).load(history.imageUri).into(binding.ivImgResultAnalyze)
+            binding.btnDelete.setOnClickListener {
+                onDeleteClick(history)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding =
+            ItemHistoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding, onDeleteClick)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val history = getItem(position)
+        holder.bind(history)
+    }
+
+    companion object {
+        val DIFF_CALLBACK: DiffUtil.ItemCallback<HistoryEntity> =
+            object : DiffUtil.ItemCallback<HistoryEntity>() {
+                override fun areItemsTheSame(
+                    oldItem: HistoryEntity,
+                    newItem: HistoryEntity
+                ): Boolean {
+                    return oldItem == newItem
+                }
+
+                override fun areContentsTheSame(
+                    oldItem: HistoryEntity,
+                    newItem: HistoryEntity
+                ): Boolean {
+                    return oldItem == newItem
+                }
+
+            }
+    }
 }
